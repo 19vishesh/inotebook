a1fd1db57b56a641cf06243a1ab3c317da3125d2
Vishesh-Kumar <19vishesh@gmail.com>
2023-01-18 19:19:01 +0530

    commit

--- BACKEND/routes/auth.js
+++ BACKEND/routes/auth.js
@@ -1,13 +1,14 @@
 const { model } = require("mongoose");
-
 const express = require('express');
+const { application } = require("express");
 const router = express.Router();
+const User = require('../models/User')
 
-router.get('/', (req, res) => {
-    obj = {
-        a: 'thios',
-        number: 34
-    }
+router.post('/', (req, res) => {
+    console.log(req.body)
+    const user = user(req.body);
+    user.save()
+    res.send(req.body)
 })
 
 module.exports = router
